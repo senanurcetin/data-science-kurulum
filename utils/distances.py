0c29bb65c22c026b587ca91b42cbccf0ea4e8080
senanur <senanur.cetin.work@gmail.com>
2026-07-06 21:26:47 +0300

    feat(utils): manhattan distance

--- utils/distances.py
+++ utils/distances.py
@@ -1,1 +1,7 @@
-pass  # YOUR CODE HERE
+%load_ext autoreload
+%autoreload 2
+def manhattan(a, b):
+    """
+    İki nokta arasındaki Manhattan mesafesini hesaplar.
+    """
+    return sum(abs(val1 - val2) for val1, val2 in zip(a, b))
